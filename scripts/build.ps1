5b804173f158527e2bcc77d9cea0b3ee07e57010
Michael Zelensky <michaelzelensky@gmail.com>
2026-06-23 09:19:52 +0700

    Debug script encoding

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -28,15 +28,19 @@ foreach ($file in $contentFiles) {
 
   if ($file.Extension -eq ".md") {
 
-    $html = & "C:\Program Files\Pandoc\pandoc.exe" `
+    $tempFragment = "$outputDirectory/temp_fragment.html"
+
+    & "C:\Program Files\Pandoc\pandoc.exe" `
       $file.FullName `
       -f markdown `
-      -t html
+      -t html `
+      -o $tempFragment
 
-    $combinedContent += $html
+    $combinedContent += Get-Content $tempFragment -Encoding UTF8
+    Remove-Item $tempFragment
   }
   else {
-    $combinedContent += Get-Content $file.FullName
+    $combinedContent += Get-Content $file.FullName -Encoding UTF8
   }
 
   $combinedContent += ""
@@ -53,13 +57,12 @@ $currentDateFormatted = $currentDate.ToString(
 
 $combinedContent = $combinedContent -replace '\$date\$', $currentDateFormatted
 
-$combinedContent | Set-Content $tempFile -Encoding UTF8
+[System.IO.File]::WriteAllLines($tempFile, $combinedContent, [System.Text.UTF8Encoding]::new($false))
 
 & "C:\Program Files\Pandoc\pandoc.exe" `
   $tempFile `
   --css="$bookRoot/styles/book.css" `
   --standalone `
-  --metadata charset=utf-8 `
   --pdf-engine="C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe" `
   -o $outputFile
 
